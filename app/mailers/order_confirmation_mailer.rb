d2fd1076e1f919afb0190ae07888511d3f9c5481
cypradoux <pradouxcyprien@gmail.com>
2023-11-29 17:29:28 +0100

    order confirmation mailer

---
+++ app/mailers/order_confirmation_mailer.rb
@@ -0,0 +1,9 @@
+class OrderConfirmationMailer < ApplicationMailer
+
+    def order_confirmation_mail(order)
+        @order = order
+        @user = order.user
+        @url = ''  
+        mail(to: @user.email, subject: 'Merci pour votre commande, voici la/les photos que vous avez acheté !')
+    end
+end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,11 @@
 class Order < ApplicationRecord
   has_many :order_items, dependent: :destroy
+  has_many :items, through: :order_items
   belongs_to :user
 
   before_destroy :destroy_associated_records
   after_create :clear_cart_after_order
+  after_create :confirmation_order_send
 
   private
 
@@ -26,5 +28,9 @@ class Order < ApplicationRecord
     cart = Cart.find_by(user_id: self.user_id)
     cart&.cart_items&.clear
     cart&.update(total_price: nil)
+  end
+  
+  def confirmation_order_send
+    OrderConfirmationMailer.order_confirmation_mail(self).deliver_now
   end
 end
